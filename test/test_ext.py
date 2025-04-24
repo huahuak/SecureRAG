3bca887c0179ca2d5af7de1b84e9154cea395a9c
huahuak <moflowerlkh@gmail.com>
2025-04-25 05:05:56 +0800

    update ext test

--- test/test_ext.py
+++ test/test_ext.py
@@ -1,7 +1,7 @@
 import random
-import unittest
 
 import torch
+import torch.nn.functional as F
 from test.test_base import TestConfigLoggerBase
 
 
@@ -19,3 +19,15 @@ class TestExt(TestConfigLoggerBase):
         tensorRef = self.srag.copyTensorToSGX(tensor)
         tensorFromSGX = self.srag.copyTensorFromSGX(tensorRef)
         self.assertTrue(torch.allclose(tensor, tensorFromSGX, atol=1e-4))
+
+    def test_linear(self):
+        N = 1024
+        indim = 1024
+        outdim = 512
+        linear = torch.nn.Linear(indim, outdim, True, dtype=torch.float32)
+        a = torch.rand((N, indim), dtype=torch.float32)
+        b = linear.weight
+        c = linear.bias
+        out = self.srag.secureLinear(a, b, c)
+        ans = F.linear(a, b, c)
+        self.assertTrue(torch.allclose(out, ans, atol=1e-4))
